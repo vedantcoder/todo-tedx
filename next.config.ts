983fb03fa5263d4fa9e3581788939b475607e058
vedantcoder <nichalvedant07@gmail.com>
2025-07-01 00:02:37 +0530

    fix deploy issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
